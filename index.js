7435bbb00a4f5118dc55474b2452c22373d81eb1
Dan Reeves <heydanreeves@gmail.com>
2015-12-14 23:13:42 +0000

    feat: hoodie.reset
    Add the ability to reset the hoodie client object to a fresh state.
    Plugins can listen to the 'reset' event in order to perform their
    own resetting.

--- index.js
+++ index.js
@@ -21,20 +21,29 @@ function Hoodie (options) {
     }
   }
 
+  api.on = require('./lib/events').on.bind(this, state)
+  api.one = require('./lib/events').one.bind(this, state)
+  api.off = require('./lib/events').off.bind(this, state)
+  api.trigger = require('./lib/events').trigger.bind(this, state)
+
   var CustomStore = Store.defaults({ remoteBaseUrl: api.url + '/store/api' })
   var dbName = 'user/' + api.id
   api.store = new CustomStore(dbName)
+  api.on('reset', function (opts) {
+    opts.promise = opts.promise.then(api.store.clear)
+  })
 
   api.account = new Account({ url: api.url + '/account/api' })
+  api.on('reset', function (opts) {
+    opts.promise = opts.promise.then(api.account.isSignedIn() ? api.account.signOut : null)
+  })
+
   api.task = new Task('/hoodie/task/api')
   api.connectionStatus = new ConnectionStatus(api.url)
   api.log = new Log('hoodie')
   api.plugin = require('./lib/plugin')
 
-  api.on = require('./lib/events').on.bind(this, state)
-  api.one = require('./lib/events').one.bind(this, state)
-  api.off = require('./lib/events').off.bind(this, state)
-  api.trigger = require('./lib/events').trigger.bind(this, state)
+  api.reset = require('./lib/reset').bind(api, state)
 
   return api
 }
