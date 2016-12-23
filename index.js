f9865ac7c726255dfb6635727adfb35817a108ed
Gregor <gregor@martynus.net>
2016-12-22 19:32:50 -0800

    feat: hoodie.ready
    BREAKING CHANGE:
    
    Hoodie dropped its dependency on localStorage in order to work in non-browser environments. In order to make that work we have to use async store APIs to load the current state like the session id, username or the connection status.
    
    The APIs `hoodie.account`, `hoodie.store` and `hoodie.connectionStatus` can now only be used after the promise returned by `hoodie.ready` resolves.

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ module.exports = Hoodie
 
 var getState = require('./lib/get-state')
 var getApi = require('./lib/get-api')
-var init = require('./lib/init')
 
 function Hoodie (options) {
   var state = getState(options)
   var api = getApi(state)
-  init(api)
+
   return api
 }
