d9e791bbcbfee50c3ee28b1f5e87a3ce11c7c809
Gregor <gregor@martynus.net>
2017-03-09 00:52:20 -0800

    test: adapt for latest `@hoodie/{account,store}-client

--- tests/specs/url.js
+++ tests/specs/url.js
@@ -1,22 +1,9 @@
-var simple = require('simple-mock')
 var test = require('tape')
 
 var Hoodie = require('../../index')
 var PouchDBMock = require('./utils/pouchdb')
 
 test('has "url" property', function (t) {
-  t.plan(3)
-
-  simple.mock(global, 'location', {
-    origin: 'http://localhost:1234'
-  })
-  var defaultHoodie = new Hoodie({
-    PouchDB: PouchDBMock
-  })
-  var hoodieDefaultUrl = 'http://localhost:1234/hoodie'
-  t.is(typeof defaultHoodie.url, 'string', 'has a url')
-  t.is(defaultHoodie.url, hoodieDefaultUrl, 'url has a default value')
-
   var exampleUrl = 'http://example.com'
   var hoodieExampleUrl = exampleUrl + '/hoodie'
   var specifiedUrlHoodie = new Hoodie({
@@ -24,6 +11,5 @@ test('has "url" property', function (t) {
     PouchDB: PouchDBMock
   })
   t.is(specifiedUrlHoodie.url, hoodieExampleUrl, 'url is retained after being passed in')
-
-  simple.restore()
+  t.end()
 })
--- tests/specs/utils/pouchdb.js
+++ tests/specs/utils/pouchdb.js
@@ -1,3 +1,4 @@
 module.exports = require('pouchdb-core')
   .plugin(require('pouchdb-replication'))
   .plugin(require('pouchdb-adapter-memory'))
+  .plugin(require('pouchdb-adapter-http'))
